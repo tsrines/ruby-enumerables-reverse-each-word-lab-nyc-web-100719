047672928fb3f34174b6e03f65eab11425448739
Timothy Rines <tsrines@gmail.com>
2019-09-25 01:50:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence = { sentence.each{ |string| string.reverse }
-  sentence
+  sentence = { sentence.each. { |string| string.reverse }
+  puts sentence
 
 end
